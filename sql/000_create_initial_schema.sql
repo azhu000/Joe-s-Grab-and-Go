2a4a1a24d50df38cdffd107ff4e286ad3799a6e7
AliMoh98 <alimohamed1398@gmail.com>
2022-02-25 19:57:36 -0500

    simplify init sql files

--- sql/000_create_initial_schema.sql
+++ sql/000_create_initial_schema.sql
@@ -136,11 +136,11 @@ DROP TABLE IF EXISTS `orderLineItem` ;
 
 CREATE TABLE IF NOT EXISTS `orderLineItem` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `orderID` INT NOT NULL,
   `quantity` VARCHAR(45) NOT NULL,
   `subtotal` VARCHAR(45) NOT NULL,
   `discount` VARCHAR(45) NULL,
   `total` VARCHAR(45) NOT NULL,
-  `orderID` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_orderID_idx` (`orderID` ASC) VISIBLE,
   CONSTRAINT `fk_orderID`
