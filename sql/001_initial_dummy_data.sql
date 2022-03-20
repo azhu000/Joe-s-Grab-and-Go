39ae357c37c5e6de9a43d783fcfd02c525ae76e1
AliMoh98 <alimohamed1398@gmail.com>
2022-03-20 17:17:36 -0400

    dummy data for database

---
+++ sql/001_initial_dummy_data.sql
@@ -0,0 +1,51 @@
+INSERT INTO businesses (Name, Address, Phone) VALUES ( "Moe's Tavern", '555 5th Avenue', '(212)-867-5309');
+INSERT INTO employees VALUES (1, "Tracy", 'Manager', 1);
+INSERT INTO employees VALUES (2, "Stefan", 'Chef', 1);
+INSERT INTO employees VALUES (3, "Rachel", 'Chef', 1);
+INSERT INTO employees VALUES (4, "Bob", 'Delivery', 1);
+INSERT INTO employees VALUES (5, "Jeffrey", 'Delivery', 1);
+
+INSERT INTO dish VALUES (1, "Caesar Salad", 'a tossed salad made of romaine, garlic, anchovies, and croutons and dressed with olive oil, coddled egg, lemon juice, and grated cheese. ', 1);
+INSERT INTO dish VALUES (2, "Flaming Moe", 'The signature specialty drink of Springfield', 1);
+INSERT INTO dish VALUES (3, "Chicken Wings", 'Fresh chicken wings that comes with a side dip of your choice.', 1);
+INSERT INTO dish VALUES (4, "Draft Beer", 'Choose from Duff or Duff Lite.', 1);
+INSERT INTO dish VALUES (5, "Mr.Teeny", 'Lemonade, pineapple juice, and Blue Curaçao syrup. Freshly prepared and served with a specialty garnish.', 1);
+INSERT INTO dish VALUES (6, "Burgers and Fries", 'A juicy hamburger topped with lettuce, pickles, onions, mayo and ketchup and a side of fries.', 1);
+INSERT INTO dish VALUES (7, "Birria Tacos", ' Topped with cilantro, onion, a spicy red sauce and a wedge of lime.', 1);
+INSERT INTO dish VALUES (8, "Pizza", 'its just pizza.', 1);
+
+INSERT INTO menu VALUES (1, 2, 1);
+INSERT INTO menu VALUES (2, 3, 1);
+INSERT INTO menu VALUES (3, 3, 1);
+INSERT INTO menu VALUES (4, 2, 1);
+INSERT INTO menu VALUES (5, 2, 1);
+
+INSERT INTO customers VALUES (1, "Jolene", "Jolene", "(347)-621-2020",0);
+INSERT INTO customers VALUES (2, "Nathaniel", "Nathaniel", "(646)-626-6686",1);
+INSERT INTO customers VALUES (3, "Tom", "Tom", "(718)-762-8213",0);
+INSERT INTO customers VALUES (4, "Angelica","Angelica", "(939)-952-6445",0);
+INSERT INTO customers VALUES (5, "Brady", "Brady", "(716)-216-6677",1);
+
+-- INSERT INTO orders VALUES (1, "$24.50", NULL, 3, 1);
+-- INSERT INTO orders VALUES (2, "$11.00", NULL, 3, 1);
+-- INSERT INTO orders VALUES (3, "$4.19", NULL, 3, 1);
+
+-- INSERT INTO orderLineItem VALUES (1, 8, 2, 3);
+-- INSERT INTO orderLineItem VALUES (1, 1, 2, 3);
+-- INSERT INTO orderLineItem VALUES (3, 3, 2, 3);
+-- INSERT INTO orderLineItem VALUES (4, "Pizza", 2, 3);
+-- INSERT INTO orderLineItem VALUES (5, "Pizza", 2, 3);
+
+-- INSERT INTO dishRating VALUES (1, 5, "Pizza", 2, 3);
+-- INSERT INTO dishRating VALUES (2, 4, "Pizza", 2, 3);
+-- INSERT INTO dishRating VALUES (3, 4, "Pizza", 2, 3);
+-- INSERT INTO dishRating VALUES (4, 5, "Pizza", 2, 3);
+-- INSERT INTO dishRating VALUES (5, 3, "Pizza", 2, 3);
+
+
+-- INSERT INTO menuDishes VALUES(1,1,$7.99)
+-- INSERT INTO menuDishes VALUES(1,3,$8.99)
+-- INSERT INTO menuDishes VALUES(2,2,$4.19)
+-- INSERT INTO menuDishes VALUES(2,3,$9.99)
+-- INSERT INTO menuDishes VALUES(2,8,$3.00)
+
