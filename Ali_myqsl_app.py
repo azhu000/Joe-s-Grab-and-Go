7596b98fcd335e9301b9196aeb779646229f9a32
azhu000 <azhu000@citymail.cuny.edu>
2022-05-05 15:35:42 -0400

    added the contact page route

--- Ali_myqsl_app.py
+++ Ali_myqsl_app.py
@@ -347,6 +347,10 @@ def register():
     #returns the register.html file
     return render_template('register.html')#, '''form=form''')
 
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    return render_template('contact_us.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
