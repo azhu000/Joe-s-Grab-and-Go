65043fd29dd75c909431add3933669767a4a3159
HyeminShin <sshin.hyemin@gmail.com>
2022-05-16 00:33:04 -0400

    js added for delivery page

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function handleSubmit() {
-    console.log(this)
+function handleButtonClick() {
+    const submitButton = document.getElementsByClassName('btn-bid-submit');
+    const inputElement = document.getElementsByClassName('input-field');
+    submitButton[0].disabled = "true";
+    inputElement[0].disabled = "true";
 }
